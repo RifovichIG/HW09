31e63ecf5257f3145987f1a6aae4f0a4e9bbf45a
IldarG <Rifovich02@gmail.com>
2023-03-12 19:08:01 +0500

    03end

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+//M = 28; N = 7 -> 7
+
+System.Console.WriteLine("Задайте числа m и n (через , или пробел):");
+char[] separators = { ' ', ',', ';' };
+int[] mn = Array.ConvertAll(Console.ReadLine()!.Split(separators), int.Parse);
+
+if (mn[0] > mn[1])
+    (mn[0], mn[1]) = (mn[1], mn[0]);
+System.Console.Write(" ");
+Task68(mn[0], mn[1]);
+void Task68(int m, int n)
+{
+    if (m == n)
+    {
+        System.Console.WriteLine("Наибольший общий делитель равен " + m);
+        return;
+    }
+    if (m > n)
+        (m, n) = (n, m);
+    Task68(m, n - m);
+}
